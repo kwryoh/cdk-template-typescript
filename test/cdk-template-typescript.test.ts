47f1e4a9d2acaa616788d90dee4675e45e86849a
Ryoh Kawai <kawairyo@rg-automated.jp>
2022-07-19 08:05:40 +0000

    Add cdk initial files

---
+++ test/cdk-template-typescript.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTemplateTypescript from '../lib/cdk-template-typescript-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-template-typescript-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTemplateTypescript.CdkTemplateTypescriptStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
